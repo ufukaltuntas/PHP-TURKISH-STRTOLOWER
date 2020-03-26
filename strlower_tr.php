baeaf50f3e923e60b9b82a99e62728405aa8069b
ufuxio <ufuxio@gmail.com>
2020-03-26 23:43:35 +0300

    Add files via upload
    echo strlower_tr('teSt cONtent'); // output  Test Content
    
    echo strlower_tr('test content'); // output  Test Content
    
    echo strlower_tr('TEST CONTENT'); // output  Test Content

---
+++ strlower_tr.php
@@ -0,0 +1,22 @@
+<?php
+function strlower_tr($string, $charset = 'UTF-8')
+{
+    $special_cases = array(
+        'Ç' => 'ç',
+        'Ş' => 'ş',
+        'Ö' => 'ö',
+        'Ğ' => 'ğ',
+        'I' => 'ı',
+    );
+    $string = str_replace(array_keys($special_cases), array_values($special_cases), $string);
+    $string = mb_strtolower($string, 'UTF-8');
+    return mb_convert_case(mb_strtolower($string), MB_CASE_TITLE, $charset);
+}
+
+echo strlower_tr('teSt cONtent'); // output  Test Content
+
+echo strlower_tr('test content'); // output  Test Content
+
+echo strlower_tr('TEST CONTENT'); // output  Test Content
+
+?>
